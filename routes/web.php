ecc957c61d73bd0dfc3ae2927107b2127a6a0e97
amani esseili <esseiliamani@hotmail.com>
2023-10-06 23:55:27 +0200

    adds routes and guest controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\ProjectController;
+use App\Http\Controllers\ProjectController as GuestProjectController;// lo importo aggiungendo as .. per nn avere problemi dello stesso nome con admin 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +23,38 @@ Route::get('/', function () {
 Route::get('/admin', function () {
     return view('admin.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+//creo le rotte per le mie CRUD:------------------------
+
+//Ragruppo le rotte aggiungendo il middleware per assicurare che queste rotte admin siano accessibili solo agli utenti loggati
+
+Route::middleware(['auth','verified'])
+    ->prefix('admin')  // per non inserire /admin in ogni rotta
+    ->name('admin.')   //per nn inserire admin.(...admin.projects.index)
+    ->group(function(){
+
+
+    //READ
+    //per l'elenco dei progetti
+    Route::get("/projects", [ProjectController::class,"index"])->name("projects.index");
+
+    //per la visualizzazione dei dettagli di un progetto
+    Route::get("/projects/{project}", [ProjectController::class,"show"])->name("projects.show");
+
+    //CREATE
+    //per il modulo di creazione di un nuovo progetto
+    Route::get("/projects/create", [ProjectController::class,"create"])->name("projects.create");
+
+    //per salvare un nuovo progetto nel data base
+    Route::post("/projects", [ProjectController::class,"store"])->name("projects.store");
+
+
+});
+
+//------------------------------------------------------
+//rotta per il guest controller-----------------------
+Route::get("/projects",[GuestProjectController::class,"index"])->name("projects.index");
+//----------------------------------------------------
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
